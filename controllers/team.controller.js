81d0dd4b7d94f9738fbd8c3e23b167463624389a
Aurélien <aurelien.strimelle@bstorm.be>
2024-03-13 11:31:30 +0100

    Add routing for Team 🏇

---
+++ controllers/team.controller.js
@@ -0,0 +1,32 @@
+const teamController =  {
+
+    getOne : async (req, res) => {
+        res.sendStatus(501);
+    },
+    
+    getAll : async (req, res) => {
+        res.sendStatus(501);
+    },
+    
+    add : async (req, res) => {
+        res.sendStatus(501);
+    },
+    
+    update : async (req, res) => {
+        res.sendStatus(501);
+    },
+    
+    remove : async (req, res) => {
+        res.sendStatus(501);
+    },
+    
+    addPlayer : async (req, res) => {
+        res.sendStatus(501);
+    },
+    
+    removePlayer : async (req, res) => {
+        res.sendStatus(501);
+    }
+};
+
+export default teamController;
